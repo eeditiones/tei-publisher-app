80eb78e1a1c88411d03ea0fcb04bf382d4ff2954
Wolfgang Meier <wolfgang@existsolutions.com>
2019-09-08 19:16:13 +0200

    Change build to work with separate tei-publisher-components package

---
+++ deps.js
@@ -0,0 +1,4 @@
+import "@webcomponents/webcomponentsjs/webcomponents-loader";
+import "@polymer/app-layout/";
+import "@polymer/paper-card";
+import "tei-publisher-components/pb-components";
---
+++ rollup.config.js
@@ -0,0 +1,19 @@
+import resolve from 'rollup-plugin-node-resolve';
+import { terser } from 'rollup-plugin-terser';
+
+// `npm run build` -> `production` is true
+// `npm run dev` -> `production` is false
+const production = !process.env.ROLLUP_WATCH;
+
+export default {
+	input: 'deps.js',
+	output: {
+		file: 'bundle.js',
+		format: 'iife', // immediately-invoked function expression — suitable for <script> tags
+		sourcemap: true
+	},
+	plugins: [
+		resolve(), // tells Rollup how to find date-fns in node_modules
+		production && terser() // minify, but only in production
+	]
+};
