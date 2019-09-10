4ed1ae78627bb0aaabf0e6f1c3df8db1b267479c
Wolfgang Meier <wolfgang@existsolutions.com>
2019-09-10 10:51:55 +0200

    Improve build configuration and get  pages to work partially

--- deps.js
+++
@@ -1,4 +0,0 @@
-import "@webcomponents/webcomponentsjs/webcomponents-loader";
-import "@polymer/app-layout/";
-import "@polymer/paper-card";
-import "tei-publisher-components/pb-components";
---
+++ resources/scripts/deps.js
@@ -0,0 +1,9 @@
+import "@webcomponents/webcomponentsjs/webcomponents-loader";
+import "@polymer/app-layout/";
+import '@polymer/paper-dialog';
+import '@polymer/paper-dialog-scrollable';
+import "@polymer/paper-card";
+import '@polymer/paper-fab';
+import '@polymer/paper-listbox';
+import '@polymer/paper-menu-button';
+import "tei-publisher-components/pb-components";
