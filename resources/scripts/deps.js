0d5928f258bb2777dbd0ef7cf4b95e07474cb0b8
Wolfgang Meier <wolfgang@existsolutions.com>
2019-11-07 12:50:17 +0100

    Update for facsimile integration

--- resources/scripts/deps.js
+++ resources/scripts/deps.js
@@ -1,10 +1,3 @@
 import '@webcomponents/webcomponentsjs/webcomponents-loader';
-import '@polymer/app-layout';
-import '@polymer/paper-dialog';
-import '@polymer/paper-dialog-scrollable';
 import '@polymer/paper-card';
-import '@polymer/paper-fab';
-import '@polymer/paper-listbox';
-import '@polymer/paper-menu-button';
-import '@polymer/paper-tabs';
-import 'tei-publisher-components/pb-components';
+import '@teipublisher/pb-components/src/pb-components.js';
