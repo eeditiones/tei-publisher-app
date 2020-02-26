50ea896da36b79308e15369f4eaf63665fe0f795
plutonik <plutonik@gmx.net>
2020-02-26 18:37:04 +0100

    chore(tests): Rename folder test to tests

--- webtest/tests/util.js
+++ webtest/tests/util.js
@@ -3,16 +3,16 @@ var fs = require('fs');
 var request = require('request');
 
 browser.addCommand("login", function() {
-    browser.waitForVisible("a[href='#loginDialog']");
+    Page.waitForVisible("a[href='#loginDialog']");
     var loggedIn = browser.isExisting("a[href='?logout=true']");
     if (!loggedIn) {
         browser.click("a[href='#loginDialog']");
-        browser.waitForVisible("#loginDialog");
+        Page.waitForVisible("#loginDialog");
         browser.setValue("input[name='user']", "tei");
         browser.setValue("input[name='password']", "simple");
         browser.submitForm("#loginDialog form");
 
-        browser.waitForVisible("*=Admin");
+        Page.waitForVisible("*=Admin");
     }
 });
 
