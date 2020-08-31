825b4a21a630be2e2f07ff5e29438804147f6288
Magdalena Turska <tuurma@gmail.com>
2020-08-31 21:10:09 +0200

    feat(tests) tests for templates

---
+++ test/templates.test.js
@@ -0,0 +1,80 @@
+const path = require('path');
+const chai = require('chai');
+const expect = chai.expect;
+const chaiResponseValidator = require('chai-openapi-response-validator');
+const axios = require('axios');
+
+const server = 'http://localhost:8080/exist/apps/tei-publisher/api/';
+
+const spec = path.resolve("./modules/lib/api.json");
+chai.use(chaiResponseValidator(spec));
+
+let axiosInstance = axios.create({
+    baseURL: server,
+    headers: {
+        "Origin": "http://localhost:8080"
+    },
+    withCredentials: true
+});
+
+let doc = [{
+    "name": "documentation.html",
+    "title": "Documentation"
+  }]
+
+describe('/api/templates [authenticated]', function () {
+
+    before(async function() {
+        console.log('Logging in user ...');
+        const res = await axiosInstance.request({
+            url: 'login',
+            method: 'post',
+            params: {
+                "user": "tei",
+                "password": "simple"
+            }
+        });
+        expect(res.status).to.equal(200);
+        expect(res.data.user).to.equal('tei');
+
+        const cookie = res.headers["set-cookie"];
+        axiosInstance.defaults.headers.Cookie = cookie[0];
+        console.log('Logged in as %s: %s', res.data.user, res.statusText);
+    });
+
+    it('retrieves a list of templates', async function () {
+        this.timeout(10000);
+        const res = await axiosInstance.get('templates');
+
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.an('array').that.has.members;
+        expect(res.data).to.include.deep.members(doc, 'documentation template not found');
+        expect(res).to.satisfyApiSpec;
+    });
+
+    after(function (done) {
+        console.log('Logging out ...');
+        axiosInstance.request({
+            url: 'login',
+            method: 'get',
+            params: {
+                "logout": "true"
+            }
+        })
+        .catch((error) => {
+            expect(error.response.status).to.equal(401);
+            done();
+        });
+    });
+});
+
+describe('/api/templates [not authenticated]', function () {
+    it('retrieves a list of templates', async function () {
+        this.timeout(10000);
+        const res = await axiosInstance.get('templates');
+        expect(res.status).to.equal(200);
+        expect(res.data).to.be.an('array').that.has.members;
+        expect(res.data).to.include.deep.members(doc, 'documentation template not found');
+        expect(res).to.satisfyApiSpec;
+    });
+});
