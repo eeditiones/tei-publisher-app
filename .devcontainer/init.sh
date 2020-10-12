16cb325f84228f102c6e7ffda3c5aadfa4576b07
Wolfgang Meier <wolfgang@exist-db.org>
2020-10-12 19:17:34 +0200

    add config for remote container

---
+++ .devcontainer/init.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+sleep 1
+cd $(dirname "$0")/..
+ant
+for XAR in build/*.xar; do
+    echo Installing $XAR
+    curl --upload-file $XAR -u 'admin:' http://localhost:8080/exist/rest/db/system/repo/init.xar
+    curl -u 'admin:' 'http://localhost:8080/exist/rest/db?_xpath=repo:install-and-deploy-from-db("/db/system/repo/init.xar")'
+done
