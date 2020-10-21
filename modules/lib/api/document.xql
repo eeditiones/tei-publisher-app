eeditiones/tei-publisher-app

commit bcac82d26bbceeeab68cd199b764295bb611de0a

Add toolbar button to go back to parent collection; closes #26

modules/lib/api/document.xql
@@ -356,11 +356,21 @@ declare function dapi:get-fragment($request as map(*)) {
                                 <output:method>html5</output:method>
                                     </output:serialization-parameters>),
                             "footnotes": $transformed?footnotes,
-                            "userParams": $userParams
+                            "userParams": $userParams,
+                            "collection": dapi:get-collection($xml?data)
                         }
                     )
         else
             error($errors:NOT_FOUND, "Document " || $doc || " not found")
+};
+
+declare function dapi:get-collection($data) {
+    let $collection := util:collection-name($data)
+    return
+        if ($collection) then
+            substring-after($collection, $config:data-root || "/")
+        else
+            ()
 };
 
 declare %private function dapi:extract-footnotes($html as element()*) {

modules/lib/pages.xql
@@ -267,10 +267,18 @@ declare function pages:get-content($config as map(*), $div as element()) {
 };
 
 declare function pages:pb-page($node as node(), $model as map(*)) {
+    let $docPath := 
+        if (matches($model?doc, "^.*/[^/]*$")) then
+            replace($model?doc, "^(.*)/[^/]*$", "$1")
+        else
+            ""
     let $model := map:merge(
         (
             $model,
-            map { "app": $config:context-path }
+            map { 
+                "app": $config:context-path,
+                "collection": $docPath
+            }
         )
     )
     return

data/collection.html
@@ -3,8 +3,8 @@
         <li class="document">
             <div class="thumbnail"><img src="demo.png" width="128"/></div>
             <div class="document-info">
-                <h3><a href="#" data-collection="test"><pb-i18n key="browse.demo.title">TEI Publisher Demo Collection</pb-i18n></a></h3>
-                <p><pb-i18n key="browse.demo.description">Documents demonstrating various features. The selection is in no way comprehensive but represents a subset of what is possible. Please contribute your own use cases to complement this.</pb-i18n></p>
+                <h3><a href="#" data-collection="test"><pb-i18n key="browse.test.title">TEI Publisher Demo Collection</pb-i18n></a></h3>
+                <p><pb-i18n key="browse.test.description">Documents demonstrating various features. The selection is in no way comprehensive but represents a subset of what is possible. Please contribute your own use cases to complement this.</pb-i18n></p>
             </div>
         </li>
         <li class="document">
@@ -17,8 +17,8 @@
         <li class="document">
             <div class="thumbnail"><img src="documentation.png"/></div>
             <div class="document-info">
-                <h3><a href="doc/documentation.xml"><pb-i18n key="browse.documentation.title">Documentation</pb-i18n></a></h3>
-                <p><pb-i18n key="browse.documentation.description">Documentation and News using Docbook XML.</pb-i18n></p>
+                <h3><a href="doc/documentation.xml"><pb-i18n key="browse.doc.title">Documentation</pb-i18n></a></h3>
+                <p><pb-i18n key="browse.doc.description">Documentation and News using Docbook XML.</pb-i18n></p>
             </div>
         </li>
     </ul>

modules/config.xqm
@@ -44,7 +44,7 @@ declare variable $config:origin-whitelist := (
  : In this case, change $config:webcomponents-cdn to point to http://localhost:port 
  : (default: 8000, but check where your server is running).
  :)
-declare variable $config:webcomponents := "1.4.0";
+declare variable $config:webcomponents := "1.5.0";
 
 (:~
  : CDN URL to use for loading webcomponents. Could be changed if you created your

resources/css/theme.css
@@ -149,6 +149,11 @@ app-drawer-layout:not([narrow]) [drawer-toggle] {
     color: var(--pb-toolbar-color);
 }
 
+.toolbar a {
+    color: inherit;
+    text-decoration: none;
+}
+
 [drawer-toggle] {
     padding-right: 0;
 }

templates/basic/templates/toolbar.html
@@ -1,5 +1,9 @@
 <app-toolbar class="toolbar" sticky="sticky">
     <paper-icon-button id="tocToggle" class="toc-toggle" icon="icons:view-list"/>
+    <a href="${app}/index.html?collection=${collection}" data-i18n="[title]browse.${collection}.title" title=""
+        data-template="pages:parse-params">
+        <paper-icon-button icon="icons:folder-open"></paper-icon-button>
+    </a>
     <pb-restricted login="login">
         <pb-ajax subscribe="transcription" emit="transcription" url="${app}/api/odd" method="post" 
             data-template="pages:parse-params" event="pb-refresh"

templates/pages/cortez-with-translation.html
@@ -69,8 +69,13 @@
             <app-header-layout>
                 <app-header slot="header" reveals="reveals" fixed="fixed" effects="waterfall">
                     <app-toolbar data-template="templates:include" data-template-path="templates/menu.html" />
+                    <!-- Overwrite toolbar to add view toggles -->
                     <app-toolbar class="toolbar" sticky="sticky">
                         <paper-icon-button id="tocToggle" class="toc-toggle" icon="icons:view-list" />
+                        <a href="${app}/index.html?collection=${collection}" data-i18n="[title]browse.${collection}.title" title=""
+                            data-template="pages:parse-params">
+                            <paper-icon-button icon="icons:folder-open"></paper-icon-button>
+                        </a>
                         <pb-toggle-feature name="norm2" selector=".choice,.choice-alternate,br" emit="transcription" subscribe="transcription">
                             <pb-i18n key="document.normalized">Normalized View</pb-i18n>
                         </pb-toggle-feature>

templates/pages/dantiscus.html
@@ -62,10 +62,15 @@
                 <app-header-layout>
                     <app-header slot="header" reveals="reveals" fixed="fixed" effects="waterfall" data-template="browse:fix-links">
                         <app-toolbar data-template="templates:include" data-template-path="templates/menu.html"/>
+                        <!-- Overwrite toolbar to add metadata toggle -->
                         <app-toolbar class="toolbar" sticky="sticky">
                             <paper-button id="tocToggle" class="toc-toggle" raised="raised">
                                 <iron-icon icon="icons:info-outline"></iron-icon>Metadata
                             </paper-button>
+                            <a href="${app}/index.html?collection=${collection}" data-i18n="[title]browse.${collection}.title" title=""
+                                data-template="pages:parse-params">
+                                <paper-icon-button icon="icons:folder-open"></paper-icon-button>
+                            </a>
                             <pb-restricted login="login">
                                 <pb-ajax subscribe="transcription" emit="transcription" url="${app}/api/odd" method="post" 
                                     data-template="pages:parse-params" event="pb-refresh"

commit 5da2ac45c69ceec10528330d5e1fca8f0274600d

Fix error introduced by bcac82d26bbceeeab68cd199b764295bb611de0a

modules/lib/api/document.xql
@@ -357,7 +357,7 @@ declare function dapi:get-fragment($request as map(*)) {
                                     </output:serialization-parameters>),
                             "footnotes": $transformed?footnotes,
                             "userParams": $userParams,
-                            "collection": dapi:get-collection($xml?data)
+                            "collection": dapi:get-collection($xml?data[1])
                         }
                     )
         else

modules/lib/pages.xql
@@ -56,7 +56,7 @@ declare variable $pages:EDIT_ODD_LINK :=
         replace($path, "/+", "/");
 
 declare function pages:pb-document($node as node(), $model as map(*)) {
-    let $odd := ($node/@odd, $model?odd) [1]
+    let $odd := ($node/@odd, $model?odd)[1]
     let $data := config:get-document($model?doc)
     let $config := tpu:parse-pi(root($data), $model?view, $odd)
     return