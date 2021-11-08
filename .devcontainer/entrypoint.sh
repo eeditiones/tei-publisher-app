f238f74ea09f4583e96ae3d4c5ac06e614cc9923
Wolfgang Meier <wolfgang@existsolutions.com>
2021-11-08 11:58:51 +0000

    Change devcontainer to automatically build and start tei-publisher-ner

---
+++ .devcontainer/entrypoint.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+cd /workspaces/tei-publisher-ner
+echo Starting Named Entity Recognition API on port 8001
+nohup python3 -m uvicorn main:app --reload --host 0.0.0.0 --port 8001 > ner.log 2>&1 &
+
+/usr/local/exist/bin/startup.sh
