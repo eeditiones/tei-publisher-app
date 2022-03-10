4d0d678b754c3139f7d5f94425dd72d5939b728a
Wolfgang Meier <wolfgang@existsolutions.com>
2022-03-10 19:24:45 +0100

    Fix DTS test

--- test/dts.test.js
+++ test/dts.test.js
@@ -29,7 +29,7 @@ describe('/api/dts/collection', function () {
     });
 
     it('navigates to child collection', async function () {
-        const res = await util.axios.get('dts/collection?id=https://teipublisher.com/dts/demo&nav=children');
+        const res = await util.axios.get('dts/collection?id=https://teipublisher.com/dts/demo&nav=children&per-page=50');
         expect(res.status).to.equal(200);
         expect(res.data['@type']).to.equal('Collection');
         expect(res.data.member.length).to.be.greaterThan(1);
