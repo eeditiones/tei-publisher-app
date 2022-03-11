f7ec029123cb4db83cebfa2364d306a09bf102a8
Wolfgang Meier <wolfgang@existsolutions.com>
2022-03-11 10:55:42 +0000

    devcontainer: use gunicorn to start tei-publisher-ner service

--- .devcontainer/python-api.sh
+++ .devcontainer/python-api.sh
@@ -2,4 +2,4 @@
 
 cd /workspaces/tei-publisher-ner
 echo Starting Named Entity Recognition API on port 8001
-nohup python3 -m spacy project run serve > ner.log 2>&1 &
+gunicorn scripts.main:app -w 2 -k uvicorn.workers.UvicornH11Worker -b 0.0.0.0:8001 --daemon
