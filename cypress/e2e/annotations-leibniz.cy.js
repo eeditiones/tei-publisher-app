1a9e833301246ab1f0fce7f1fb7797fd0048a14e
Joern Turner <joern@existsolutions.com>
2024-02-29 12:49:10 +0100

    first test bit more refined and separated

---
+++ cypress/e2e/annotations-leibniz.cy.js
@@ -0,0 +1,9 @@
+describe('leibniz spec', () => {
+
+
+  it('opens Leibniz example', () => {
+    cy.visit('http://localhost:8080/exist/apps/tei-publisher/annotate/test.xml?view=div&odd=annotations')
+  })
+
+
+})
